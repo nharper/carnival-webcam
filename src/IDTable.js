74c9053b45e9f13ce5848be3d8779efe89885bc8
Nick Harper <nharper@nharper.org>
2016-04-11 07:53:14 +0000

    Implement max_rows for IDTable; display rows in IDTable in correct order

--- src/IDTable.js
+++ src/IDTable.js
@@ -11,12 +11,17 @@ module.exports = React.createClass({
     return {max_rows: 15};
   },
   getInitialState: function() {
-    return {ids: []};
+    return {ids: [], counter: 0};
   },
   addId: function(id_obj) {
     id_obj.time = PghTime();
+    id_obj.key = this.state.counter;
+    this.setState({counter: this.state.counter + 1});
     var ids = this.state.ids;
-    ids.push(id_obj);
+    ids.unshift(id_obj);
+    while (ids.length > this.props.max_rows) {
+      ids.pop();
+    }
     this.setState({ids: ids});
   },
   componentDidMount: function() {
@@ -25,7 +30,7 @@ module.exports = React.createClass({
   render: function() {
     var ids = this.state.ids.map(function(id_obj) {
       return (
-        <tr key={id_obj.time}>
+        <tr key={id_obj.key}>
           <td>{id_obj.time}</td><td>{this.props.id_store.prettyPrint(id_obj)}</td>
         </tr>
       );
