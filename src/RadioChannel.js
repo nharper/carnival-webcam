e41c936e673e1ca1e302638ab64b3345283bbcf1
Nick Harper <nharper@nharper.org>
2018-04-12 21:23:28 +0000

    Remove MDC decoding and update package.json

--- src/RadioChannel.js
+++ src/RadioChannel.js
@@ -1,5 +1,3 @@
-var MdcDecoder = require('./MdcDecoder');
-
 module.exports = function(path, context) {
   this.audio_ = new Audio(path);
   this.audio_.crossOrigin = "anonymous";
@@ -9,70 +7,11 @@ module.exports = function(path, context) {
   var left_channel = context.createGain();
   var right_channel = context.createGain();
 
-  // Create MDC decoder.
-  this.decoder_ = new MdcDecoder(context.sampleRate);
-  this.setCallback = this.decoder_.setCallback.bind(this.decoder_);
-
-  // Create simple script processor audio node to copy samples into
-  // MDC decoder.
-  var scriptProcessor = context.createScriptProcessor(4096, 1, 1);
-  var last_input = null;
-  scriptProcessor.onaudioprocess = function(audioEvent) {
-    try {
-      var input = audioEvent.inputBuffer.getChannelData(0);
-      if (audioEvent.outputBuffer.copyToChannel) {
-        audioEvent.outputBuffer.copyToChannel(input, 0, 0);
-      } else {
-        var output = audioEvent.outputBuffer.getChannelData(0);
-        for (var i = 0; i < input.length; i++) {
-          output[i] = input[i];
-        }
-      }
-      this.decoder_.processSamples(input);
-      if (input === last_input) {
-        console.log('Inputs are equal! We might be stuck in a loop. ' + JSON.stringify(audioEvent));
-      }
-      last_input = input;
-    } catch (e) {
-      console.log(e);
-    }
-  }.bind(this);
-
-  // temporary debugging stuff
-  var log = function(event) {
-    console.log(this);
-    console.log(event);
-  }.bind(this);
-  [
-    'playing',
-    'waiting',
-    'seeking',
-    'ended',
-    'loadedmetadata',
-    'loadeddata',
-    'canplay',
-    'canplaythrough',
-    'duration',
-    'durationchange',
-    'timeupdate',
-    'play',
-    'pause',
-    'ratechange',
-    'volumechange',
-    'suspend',
-    'emptied',
-    'stalled',
-    'complete'
-  ].forEach(function(event) {
-    scriptProcessor.addEventListener(event, log);
-  });
-
   // Create audio node from audio tag and route all audio nodes together.
   try {
     var source = context.createMediaElementSource(this.audio_);
-    source.connect(scriptProcessor, 0);
-    scriptProcessor.connect(left_channel, 0);
-    scriptProcessor.connect(right_channel, 0);
+    source.connect(left_channel, 0);
+    source.connect(right_channel, 0);
     var merger = context.createChannelMerger(2);
     left_channel.connect(merger, 0, 0);
     right_channel.connect(merger, 0, 1);
